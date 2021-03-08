04a0eb89e7859f17efe10ac33b28e56e429eb8c6
Kudret Davudov <bordobereli435@gmail.com>
2021-03-08 13:36:10 +0200

    add checkout page

--- frontend/src/pages/Checkout.js
+++ frontend/src/pages/Checkout.js
@@ -1,49 +1,116 @@
-import React, { useState, useEffect } from 'react';
-import { Image, List, Spin, Card } from 'antd';
+import React, { useRef, useState, useEffect } from 'react';
+import { Image, Table, Button } from 'antd';
 import { useSelector } from 'react-redux';
 
-const { Item } = List;
+import './_checkout.less';
+
+const columns = [
+  {
+    title: '#',
+    dataIndex: 'id',
+    key: 'id',
+    render(id) {
+      return <span style={{ fontSize: 16 }}>{id}</span>;
+    },
+  },
+  {
+    title: 'Image',
+    dataIndex: 'image',
+    key: 'image',
+    render(image) {
+      return (
+        <Image
+          src={image}
+          style={{ border: '1px solid #d9d9d9', borderRadius: 10, maxWidth: 200 }}
+        />
+      );
+    },
+  },
+  {
+    title: 'Title',
+    dataIndex: 'title',
+    key: 'title',
+    render(title) {
+      return <span style={{ fontSize: 20 }}>{title}</span>;
+    },
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price',
+    render(price) {
+      return <span style={{ fontSize: 20 }}>{price}</span>;
+    },
+  },
+  {
+    title: 'Quantity',
+    dataIndex: 'quantity',
+    key: 'quantity',
+    rendeR(quantity) {
+      return <span style={{ fontSize: 20 }}>{quantity}</span>;
+    },
+  },
+];
 
 const Checkout = () => {
   const { cart } = useSelector((state) => state);
 
+  const total = useRef(0);
+
   const [data, setData] = useState(null);
 
   useEffect(() => {
+    total.current = 0;
+
     if (cart) {
-      setData(cart);
+      const data = cart.map((cart, idx) => {
+        total.current += cart.price;
+
+        return {
+          key: idx,
+          id: cart.id,
+          image: cart.image,
+          title: cart.title,
+          price: new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
+            cart.price
+          ),
+          quantity: cart.quantity,
+        };
+      });
+
+      setData(data);
     }
   }, [cart]);
 
-  if (data) {
+  const renderFooter = () => {
     return (
-      <Card className="d-table p-2" style={{ margin: 'auto', marginTop: 100, marginBottom: 100 }}>
-        <List
-          itemLayout="vertical"
-          dataSource={data}
-          renderItem={(item) => (
-            <Item className="d-flex justify-content-between align-items-center">
-              <Image
-                src={item.image}
-                alt="product"
-                style={{ minWidth: 150, width: '10vw', border: '1px solid #f0f0f0' }}
-              />
-              <div
-                className="d-flex flex-column justify-content-evenly align-items-center"
-                style={{ width: '100%', height: '100%' }}
-              >
-                <h4>{item.title}</h4>
-                <span>Quantity: {item.quantity}</span>
-                <span>Price: {item.price}</span>
-              </div>
-            </Item>
-          )}
-        />
-      </Card>
+      <div className="d-flex justify-content-between align-items-center">
+        <Button className="float-left" size="large">
+          Continue To Shipping Address
+        </Button>
+        <span className="float-right" style={{ fontSize: 18, fontWeight: 500 }}>
+          {total.current &&
+            `Total: ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
+              total.current
+            )}`}
+        </span>
+      </div>
     );
-  } else {
-    return <Spin spinning={!data} />;
-  }
+  };
+
+  return (
+    <div className="p-3" style={{ margin: 100, border: '1px solid #d9d9d9', borderRadius: 10 }}>
+      <Table
+        className="checkout-table"
+        loading={!data}
+        columns={columns}
+        dataSource={data}
+        pagination={{ pageSize: 2 }}
+        footer={renderFooter}
+        bordered
+      />
+    </div>
+  );
 };
 
 export default Checkout;
